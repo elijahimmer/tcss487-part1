cc8820a60726f57db4c4d717e13c9fd44c7ce583
Elijah M. Immer <me@eimmer.me>
2025-10-21 16:33:02 -0700

    Mostly set up for keccak implementation

--- sha3shake.java
+++ sha3shake.java
@@ -1,4 +1,18 @@
 public class SHA3SHAKE {
+  /**
+  * The buffer size in bytes
+  */
+  private static final int BUFFER_LEN = 200;
+
+  /**
+  * The data buffer, must be BUFFER_LEN long
+  */
+  private final byte[] buffer = null;
+  /**
+  * The digest length for the output
+  */
+  private int digest_len;
+
   public SHA3SHAKE() {}
 
   /**
@@ -7,6 +21,15 @@ public class SHA3SHAKE {
   * @param suffix SHA-3/SHAKE suffix (SHA-3 digest bitlength = suffix, SHAKE sec level = suffix)
   */
   public void init(int suffix) {
+    if (this.buffer != null)
+      this.buffer = new byte[BUFFER_LEN];
+    assert this.buffer.length == BUFFER_LEN;
+
+    // I think alloc in java zero initializes, but just make sure
+    for (int i = 0; i < BUFFER_LEN; i++)
+      this.buffer[i] = 0;
+
+    this.digest_len = suffix;
   }
 
   /**
@@ -17,6 +40,23 @@ public class SHA3SHAKE {
   * @param len byte count on the buffer
   */
   public void absorb(byte[] data, int pos, int len) {
+    assert data != null;
+    assert this.buffer != null;
+
+    // TODO(Elijah): Don't realloc, generate padding bytes and add them to the buffer
+    final byte[] padded = pad(data, pos, len, this.digest_length << 3);
+    assert (padded.length % this.digest_length) == 0;
+
+    int i, j;
+    for (i = pos; i < len; i += 1) {
+      this.buffer[j] ^= data[i];
+      j += 1;
+      if (j >= this.digest_length) {
+        j = 0;
+        keccak();
+      }
+    }
+    assert j == 0, "padded message must be a multiple of the digest length";
   }
 
   /**
@@ -26,7 +66,7 @@ public class SHA3SHAKE {
   * @param len byte count on the buffer (starting at index 0)
   */
   public void absorb(byte[] data, int len) {
-    absorb(data, 0, len);
+    abosrb(data, 0, len);
   }
 
   /**
@@ -113,41 +153,70 @@ public class SHA3SHAKE {
   * 10*1 pads the input string to a multiple of `x` bits.
   * 
   * @param n The message to pad
+  * @param pos initial index to start padding from
+  * @param len byte count on the buffer
   * @param x The block size, must be a multiple of 8.
   * @return The padded buffer
   */
-  private static byte[] pad(byte[] n, int x) {
+  private static byte[] pad(byte[] n, int pos, int len, int x) {
     assert (x & 0b111) == 0; // x must be a multiple of 8.
     final int x_bytes = x >> 3;
 
-    final int bytes_padding = x_bytes - (n.length % x_bytes);
+    final int bytes_padding = x_bytes - (len % x_bytes);
     assert bytes_padding > 0;
 
-    final int message_length = n.length + bytes_padding;
+    final int message_length = len + bytes_padding;
     assert (message_length % x_bytes) == 0;
 
     final byte[] ret = new byte[message_length];
-<<<<<<< Updated upstream
-    System.arraycopy(n,
-                     0,
-                     ret,
-                     0,
-=======
     System.arraycopy(ret, 0,
-                     n,   0,
->>>>>>> Stashed changes
-                     n.length);
+                     n,   pos,
+                     len);
 
     if (bytes_padding == 1) {
-      assert ret.length == n.length + 1;
-      ret[n.length] = 0b1000_0001;
+      assert ret.length == len + 1;
+      ret[len] = 0b1000_0001;
     } else {
-      ret[n.length] = 0b1000_0000;
+      ret[len] = 0b1000_0000;
 
-      for (int i = n.length + 1; i < ret.length - 1; i++)
+      for (int i = len + 1; i < ret.length - 1; i++)
         ret[i] = 0;
       
       ret[ret.length - 1] = 0b000_0001;
     }
+  }
+
+  /**
+  * Scrambles the buffer using the sha-3 keccak algorithm
+  */
+  private void keccak() {
+    for (int i = 0; i < KECCAK_ROUNDS; i += 1)
+      rnd(i);
+  };
+
+  /**
+  * One round of the sha-3 keccak algorithm
+  */
+  private void rnd(int index) {
+    transformTheta();
+    transformRho();
+    transformPi();
+    transformChi();
+    transformIota(index);
+  }
+
+  private void transformTheta() {
+  }
+
+  private void transformRho() {
+  }
+
+  private void transformPi() {
+  }
+
+  private void transformChi() {
+  }
+
+  private void transformIota(int i) {
   }
 }
