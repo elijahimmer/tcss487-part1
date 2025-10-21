ec5272558e5bc7cf4f65332aba07131f04c6d852
Elijah M. Immer <me@eimmer.me>
2025-10-21 13:33:17 -0700

    Add basic function implementations

--- sha3shake.java
+++ sha3shake.java
@@ -6,6 +6,7 @@ public class SHA3SHAKE {
   * @param suffix SHA-3/SHAKE suffix (SHA-3 digest bitlength = suffix, SHAKE sec level = suffix)
   */
   public void init(int suffix) { /* ... */ }
+
   /**
   * Update the SHAKE sponge with a byte-oriented data chunk.
   *
@@ -14,19 +15,26 @@ public class SHA3SHAKE {
   * @param len byte count on the buffer
   */
   public void absorb(byte[] data, int pos, int len) { /* ... */ }
+
   /**
   * Update the SHAKE sponge with a byte-oriented data chunk.
   *
   * @param data byte-oriented data buffer
   * @param len byte count on the buffer (starting at index 0)
   */
-  public void absorb(byte[] data, int len) { /* ... */ }
+  public void absorb(byte[] data, int len) {
+    abosrb(data, 0, len);
+  }
+
   /**
   * Update the SHAKE sponge with a byte-oriented data chunk.
   *
   * @param data byte-oriented data buffer
   */
-  public void absorb(byte[] data) { /* ... */ }
+  public void absorb(byte[] data) {
+    absorb(data, 0, data.length);
+  }
+
   /**
   * Squeeze a chunk of hashed bytes from the sponge.
   * Call this method as many times as needed to extract the total desired number of bytes.
@@ -36,6 +44,7 @@ public class SHA3SHAKE {
   * @return the val buffer containing the desired hash value
   */
   public byte[] squeeze(byte[] out, int len) { /* ... */ }
+
   /**
   * Squeeze a chunk of hashed bytes from the sponge.
   * Call this method as many times as needed to extract the total desired number of bytes.
@@ -43,20 +52,31 @@ public class SHA3SHAKE {
   * @param len desired number of squeezed bytes
   * @return newly allocated buffer containing the desired hash value
   */
-  public byte[] squeeze(int len) { /* ... */ }
+  public byte[] squeeze(int len) {
+    final byte[] out = new byte[len];
+    squeeze(out, len);
+    return out;
+  }
+
   /**
   * Squeeze a whole SHA-3 digest of hashed bytes from the sponge.
+  *
+  * NOTE(Elijah): Not sure what this is
   *
   * @param out hash value buffer
   * @return the val buffer containing the desired hash value
   */
   public byte[] digest(byte[] out) { /* ... */ }
+
   /**
   * Squeeze a whole SHA-3 digest of hashed bytes from the sponge.
   *
   * @return the desired hash value on a newly allocated byte array
   */
-  public byte[] digest() { /* ... */ }
+  public byte[] digest() {
+
+  }
+
   /**
   * Compute the streamlined SHA-3-<224,256,384,512> on input X.
   *
@@ -66,6 +86,7 @@ public class SHA3SHAKE {
   * @return the out buffer containing the desired hash value.
   */
   public static byte[] SHA3(int suffix, byte[] X, byte[] out) { /* ... */ }
+
   /**
   * Compute the streamlined SHAKE-<128,256> on input X with output bitlength L.
   *
@@ -76,4 +97,5 @@ public class SHA3SHAKE {
   * @return the out buffer containing the desired hash value.
   */
   public static byte[] SHAKE(int suffix, byte[] X, int L, byte[] out) { /* ... */ }
+
 }
