22554e0a880ab0f0de98f65224e1bf1bd90aac24
Khalid Rashid <ksrashid24@gmail.com>
2025-12-02 16:31:38 -0800

    merged changes

--- Edwards.java
+++ Edwards.java
@@ -10,6 +10,7 @@ public class Edwards {
 * Create an instance of the default curve NUMS-256.
 */
 public Edwards() { /* ... */ }
+
 /**
 * Determine if a given affine coordinate pair P = (x, y)
 * defines a point on the curve.
@@ -19,6 +20,7 @@ public Edwards() { /* ... */ }
 * @return whether P is really a point on the curve
 */
 public boolean isPoint(BigInteger x, BigInteger y) { /* ... */ }
+
 /**
 * Find a generator G on the curve with the smallest possible
 * y-coordinate in absolute value.
@@ -26,6 +28,7 @@ public boolean isPoint(BigInteger x, BigInteger y) { /* ... */ }
 * @return G.
 */
 public Point gen() { /* ... */ }
+
 /**
 * Create a point from its y-coordinate and
 * the least significant bit (LSB) of its x-coordinate.
@@ -36,6 +39,7 @@ public Point gen() { /* ... */ }
 * otherwise the neutral element O = (0, 1)
 */
 public Point getPoint(BigInteger y, boolean x_lsb) { /* ... */ }
+
 /**
 * Display a human-readable representation of this curve.
 *
@@ -45,90 +49,138 @@ public Point getPoint(BigInteger y, boolean x_lsb) { /* ... */ }
 * and p is the order of the underlying finite field F_p.
 */
 public String toString() { /* ... */ }
+
 /**
 * Edwards curve point in affine coordinates.
 * NB: this is a nested class, enclosed within the Edwards class.
 */
 public class Point {
-/**
-* Create a copy of the neutral element on this curve.
-*/
-public Point() { /* ... */ }
-/**
-* Create a point from its coordinates (assuming
-* these coordinates really define a point on the curve).
-*
-* @param x the x-coordinate of the desired point
-* @param y the y-coordinate of the desired point
-*/
-private Point(BigInteger x, BigInteger y) { /* ... */ }
-/**
-* Determine if this point is the neutral element O on the curve.
-*
-* @return true iff this point is O
-*/
-public boolean isZero() { /* ... */ }
-/**
-* Determine if a given point P stands for
-* the same point on the curve as this.
-*
-* @param P a point (presumably on the same curve as this)
-* @return true iff P stands for the same point as this
-*/
-public boolean equals(Point P) { /* ... */ }
-/**
-* Given a point P = (x, y) on the curve,
-* return its opposite -P = (-x, y).
-*
-* @return -P
-*/
-public Point negate() { /* ... */ }
-/**
-* Add two given points on the curve, this and P.
-*
-* @param P a point on the curve
-* @return this + P
-*/
-public Point add(Point P) { /* ... */ }
-/**
-* Multiply a point P = (x, y) on the curve by a scalar m.
-*
-* @param m a scalar factor (an integer mod the curve order)
-* @return m*P
-*/
-public Point mul(BigInteger m) {
-    m = m.mod(r);
-    if (m.signum() == 0) {
-        return new Point();
+    public final BigInteger x;
+    public final BigInteger y;
+    
+    /**
+    * Create a copy of the neutral element on this curve.
+    */
+    public Point() {
+      /* ... */
+      this.x = BigInteger.ZERO;      
+      this.y = BigInteger.ONE;
     }
-    Point V = new Point();
-    Point P = this;
+    
+    /**
+    * Create a point from its coordinates (assuming
+    * these coordinates really define a point on the curve).
+    *
+    * @param x the x-coordinate of the desired point
+    * @param y the y-coordinate of the desired point
+    */
+    private Point(BigInteger x, BigInteger y) { /* ... */
+      this.x = x;
+      this.y = y;
+    }
+    
+    /**
+    * Determine if this point is the neutral element O on the curve.
+    *
+    * @return true iff this point is O
+    */
+    public boolean isZero() { /* ... */
+      return this.x.equals(BigInteger.Zero) && y.equals(BigInteger.ONE);
+    }
+    
+    /**
+    * Determine if a given point P stands for
+    * the same point on the curve as this.
+    *
+    * @param P a point (presumably on the same curve as this)
+    * @return true iff P stands for the same point as this
+    */
+    public boolean equals(Point P) { /* ... */
+      return this.x.equal(P.x) && this.y.equals(P.y) 
+    }
+    
+    /**
+    * Given a point P = (x, y) on the curve,
+    * return its opposite -P = (-x, y).
+    *
+    * @return -P
+    */
+    public Point negate() {
+      BigInteger negateX = x.negate().mod(p);
+      return new Point(negateX, y);
+    }
+    
+    /**
+    * Add two given points on the curve, this and P.
+    *
+    * @param P a point on the curve
+    * @return this + P
+    */
+    public Point add(Point P) {
+      BigInteger x1 = this.x;
+      BigInteger y1 = this.y;
+      BigInteger x2 = P.x;
+      BigInteger y2 = P.y;
+    
+      BigInteger num1 = (x1.multiply(y2)).add(y1.multiply(x2));
+      BigInteger num2 = (y1.multiply(y2)).subtract(x1.multiply(x2));
+      
+      BigInteger denom1 = BigInteger.ONE.add(d.multiply(x1.multiply(x2.multiply(y1.multiply(y2)))));
+      BigInteger denom2 = BigInteger.ONE.subtract(d.multiply(x1.multiply(x2.multiply(y1.multiply(y2)))));
+      BigInteger x3 = num1.multiply(denom1.modInverse(p)).mod(p);
+      BigInteger y3 = num2.multiply(denom2.modInverse(p)).mod(p);
 
-    for (int i = m.bitLength() - 1; i >= 0; i--) {
-        V = V.add(V);
-        if (m.testBit(i)) {
-            V = V.add(P);
+      return new Point(x3, y3);
+    }
+    
+    /**
+    * Multiply a point P = (x, y) on the curve by a scalar m.
+    *
+    * @param m a scalar factor (an integer mod the curve order)
+    * @return m*P
+    */
+    public Point mul(BigInteger m) {
+        m = m.mod(r);
+        if (m.signum() == 0) {
+            return new Point();
         }
+        Point V = new Point();
+        Point P = this;
+    
+        for (int i = m.bitLength() - 1; i >= 0; i--) {
+            V = V.add(V);
+            if (m.testBit(i)) {
+                V = V.add(P);
+            }
+        }
+        return V;
     }
-    return V;
-}
-/**
-* Display a human-readable representation of this point.
-*
-* @return a string of form "(x, y)" where x and y are
-* the coordinates of this point
-*/
-public static BigInteger sqrt(BigInteger v, BigInteger p, boolean lsb) {
-    assert (p.testBit(0) && p.testBit(1));
-    if (v.signum() == 0) {
-        return BitInteger.ZERO;
+        
+    /**
+    * Display a human-readable representation of this point.
+    *
+    * @return a string of form "(x, y)" where x and y are
+    * the coordinates of this point
+    */
+    public static BigInteger sqrt(BigInteger v, BigInteger p, boolean lsb) {
+        assert (p.testBit(0) && p.testBit(1));
+        if (v.signum() == 0) {
+            return BitInteger.ZERO;
+        }
+        BigInteger r = v.modPow(p.shiftRIght(2).add(BigInteger.ONE), p);
+        if (r.testBit(0) != lsb) {
+            r = p.subtract(r);
+        }
+        return (r.multiply(r).subtract(v).mod(p).signum() == 0) ? r : null;
     }
-    BigInteger r = v.modPow(p.shiftRIght(2).add(BigInteger.ONE), p);
-    if (r.testBit(0) != lsb) {
-        r = p.subtract(r);
+    /**
+    * Display a human-readable representation of this point.
+    *
+    * @return a string of form "(x, y)" where x and y are
+    * the coordinates of this point
+    */
+    public String toString() { 
+      return "(" + x + ", " + "y" + ")";
     }
-    return (r.multiply(r).subtract(v).mod(p).signum() == 0) ? r : null;
-}
-public String toString() { /* ... */ }
-}
+  }
 }
